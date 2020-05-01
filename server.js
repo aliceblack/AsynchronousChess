11c39774c287f3624123338de37b6817e3c264f2
aliceblack <blackfluid44@gmail.com>
2020-05-01 14:07:29 +0200

    API

--- server.js
+++ server.js
@@ -62,10 +62,34 @@ app.post("/api/item", function(req, res) {
 });
 
 app.get("/api/item/:id", function(req, res) {
+    db.collection(ITEMS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+        if (err) {
+          handleError(res, err.message, "Failed to get item");
+        } else {
+          res.status(200).json(doc);
+        }
+    });
 });
 
 app.put("/api/item/:id", function(req, res) {
+    var updateDoc = req.body;
+    delete updateDoc._id;
+    db.collection(ITEMS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to update item");
+      } else {
+        updateDoc._id = req.params.id;
+        res.status(200).json(updateDoc);
+      }
+    });
 });
 
 app.delete("/api/item/:id", function(req, res) {
+    db.collection(ITEMS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+        if (err) {
+          handleError(res, err.message, "Failed to delete item");
+        } else {
+          res.status(200).json(req.params.id);
+        }
+    });
 });
